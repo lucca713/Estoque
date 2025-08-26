f55ad13be68d92bbdcf880b1d3594122012aa196
Lucca <cerasommalucca@gmail.com>
2025-08-26 20:23:10 -0300

    Finalizando ex de aumento de salario

---
+++ Ex_aumento_salario_poo/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_aumento_salario_poo
+{
+    internal class Funcionario
+    {
+        public string Nome {  get; set; }
+
+        public double SalarioBruto { get; set; }
+
+        public double Imposto { get; set; } 
+
+        public double SalarioLiquido(double imposto)
+        {
+            return  SalarioBruto -= imposto;
+        }
+
+        public void AumentoDeSalario(double qtd_aumento) { 
+             SalarioBruto += SalarioBruto * (qtd_aumento/100);
+        }
+
+        public override string ToString() {
+            return $"{Nome}, Salario de: {SalarioBruto}";
+        }
+    }
+}
---
+++ Ex_aumento_salario_poo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex_aumento_salario_poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando funcionario
+            Funcionario funcionario = new Funcionario();
+
+            //entrada dos dados
+            Console.WriteLine("Digite o nome");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salario bruto");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do imposto");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            //calculo salario liquido
+            funcionario.SalarioLiquido(funcionario.Imposto);
+
+            Console.WriteLine($"Funcionario: {funcionario}");
+
+            //calculo aumento de salario
+
+            Console.WriteLine("Digite o aumento do salario");
+            double Aumento = double.Parse(Console.ReadLine());
+            funcionario.AumentoDeSalario(Aumento);
+
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+
+        }
+    }
+}
