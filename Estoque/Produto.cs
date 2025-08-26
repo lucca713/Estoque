381f3cceadff4185b4c77e8281484f2c08b4e216
Lucca <cerasommalucca@gmail.com>
2025-08-26 18:58:24 -0300

    Mudanca no programa

---
+++ Estoque/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    internal class Produto
+    {
+        public string Nome_produto {get;set;}
+        public double Preco {get;set;}
+        public int Qtd_estoque {get;set;}
+
+        public double ValorTotalEmEstoque() { 
+            return Preco * Qtd_estoque;
+
+        }
+    }
+}
--- Estoque/Program.cs
+++ Estoque/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Estoque
+﻿using System.Globalization;
+namespace Estoque
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Produto produto = new Produto();
+
+            Console.WriteLine("Digite o nome do produto: ");
+            produto.Nome_produto = Console.ReadLine();
+
+            Console.WriteLine("Preco R$ do produto: ");
+            produto.Preco= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a quantidade em estoque: ");
+            produto.Qtd_estoque = int.Parse(Console.ReadLine());
         }
     }
 }
