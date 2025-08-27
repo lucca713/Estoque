c4b3f4e4a28444398288025b3a22f43b281012a5
Lucca <cerasommalucca@gmail.com>
2025-08-26 21:32:17 -0300

    Iniciando construtores

---
+++ Aula_contrutores/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_contrutores
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+
+        public int Quantidade { get; set; }
+
+        //construtores
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco):this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            //pois quantidade nao foi instanciado no contrutor de cima
+            Quantidade = quantidade;
+        }
+    }
+}
---
+++ Aula_contrutores/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula_contrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
