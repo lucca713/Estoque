d59393cfc57c85b6c1ef4858c209b40211632939
Lucca <cerasommalucca@gmail.com>
2025-08-27 18:15:19 -0300

    Terminando projeto de conta bancaria

--- Ex_encapsulamento_conta_bancaria/Conta_bancaria.cs
+++ Ex_encapsulamento_conta_bancaria/Conta_bancaria.cs
@@ -8,5 +8,42 @@ namespace Ex_encapsulamento_conta_bancaria
 {
     internal class Conta_bancaria
     {
+        //ja encapsulados
+        public  int Id { get; private set; }
+        public string Name { get; private set; }
+        public double Valor { get; private set; }
+
+        //construtor
+        public Conta_bancaria()
+        {
+            Valor = 0.0;
+        }
+
+        public Conta_bancaria(int id, string name):this()
+        {
+            Id = id;
+            Name = name;
+            
+        }
+
+        public Conta_bancaria(int id, string name, double valor) : this(id, name)
+        {
+            Valor = valor;
+        }
+
+        public void AddConta(double valor)
+        {
+
+            Valor += (valor - 5);
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"Conta {Id}, Titular: {Name}, Saldo: R$ {Valor}";
+        }
+
     }
+
 }
--- Ex_encapsulamento_conta_bancaria/Program.cs
+++ Ex_encapsulamento_conta_bancaria/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Ex_encapsulamento_conta_bancaria
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Ex_encapsulamento_conta_bancaria
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite o numero da conta");
+            var id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do titular da conta");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("havera deposito primeiro? ");
+            var choise = Console.ReadLine();
+
+            if (choise == "N")
+            {
+                Conta_bancaria conta = new Conta_bancaria(id, nome);
+
+                Console.WriteLine(conta);
+            }
+            else {
+
+                Console.WriteLine("informe o deposito");
+                var valor_deposito = double.Parse(Console.ReadLine());
+
+                Conta_bancaria conta = new Conta_bancaria(id, nome, valor_deposito);
+
+                Console.WriteLine(conta);
+
+                Console.WriteLine("entre com um valor para Adicionar na conta: ");
+                var valor_para_depositar = double.Parse(Console.ReadLine());
+
+                conta.AddConta(valor_para_depositar);
+                Console.WriteLine(conta);
+            }
+
+            
+
+            
         }
     }
 }
